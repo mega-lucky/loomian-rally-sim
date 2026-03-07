c397967783371d05c88e2b8a09f618ee40640c71
Chinedu Nwodo <chinedunwodojr@gmail.com>
2026-03-07 11:33:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Loomian Rally Simulator
+
+This project is intented to simulate rallying from the Roblox game Loomian Legacy.
+This project uses Typescript and React code that is transpiled to Javascript.
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx"
+
+const root: Element | null = document.getElementById("root");
+
+if (!root) {
+    throw console.error("Root not found.");
+}
+
+createRoot(root).render(<App />);
